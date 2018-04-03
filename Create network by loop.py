15e225310e7e67b708f1f51b72c70c0de217cb4b
HYH07007 <35869256+HYH07007@users.noreply.github.com>
2018-04-03 10:30:21 +0800

    First try of creating networks by loop

---
+++ Create network by loop.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+def get_veight(shape,lam):
+    # Declare a variable as weight.
+    var=tf.Variable(tf.truncated_normal(shape=shape,stddev=0.1),dtype=tf.float32)
+    # Add the regularization loss to the collection 'losses'.
+    tf.add_to_collection('losses',tf.contrib.layers.l2_regularizer(lam)(var))
+    return var
+
+x=tf.placeholder(tf.float32,shape=(None,2))
+y_=tf.placeholder(tf.float32,shape=(None,1))
+batch_size=8
+
+# The number of nodes in each layer.
+layer_dimension=[2,10,10,10,1]
+
+# The number of layers.
+n_layers=len(layer_dimension)
+
+cur_layer=x
+in_dimension=layer_dimension[0]
+
+# Using a loop to create neutral network.
+for i in range(1,n_layers):
+    out_dimension=layer_dimension[i]
+    weight=get_weight([indimension,out_dimension],0.001)
+    bias=tf.Variable(tf.constant(0.1,shape=[out_dimension]))
+    cur_layer=tf.nn.relu(tf.matmul(cur_layer,weight)+bias)
+    in_dimension=layer_dimension[i]
+
+mse_loss=tf.reduce_mean(tf.square(y_-cur_layer))
+
+tf.add_to_collection('losses',mse_loss)
+
+loss=tf.add_n(tf.get_collection('losses'))
