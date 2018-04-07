11288c26e76aedd3b9acbbc74181724007eb1d4e
HYH07007 <35869256+HYH07007@users.noreply.github.com>
2018-04-07 18:48:06 +0800

    Denoising Autoencoder

--- Week3 Denoising Autoencoder.py
+++ Week3 Denoising Autoencoder.py
@@ -32,40 +32,40 @@ class AdditiveGaussianNoiseAutoencoder(object):
         self.sess=tf.Session()
         self.sess.run(init)
 
-        # Declaring a dictionary named all_weights and return it, which contains weights and biasses.
-        def _initialize_weights(self):
-            all_weights=dict()
-            all_weights['w1']=tf.Variable(xavier_init(self.n_input,self.n_hidden))
-            all_weights['b1']=tf.Variable(tf.zeros([self.n_hidden],dtype=tf.float32))
-            all_weights['w2']=tf.Variable(tf.zeros([self.n_hidden,self.n_input],dtype=tf.float32))
-            all_weights['b2']=tf.Varibale(tf.zeros([self.n_input],dtype=tf.float32))
-            return all_weights
+    # Declaring a dictionary named all_weights and return it, which contains weights and biasses.
+    def _initialize_weights(self):
+        all_weights=dict()
+        all_weights['w1']=tf.Variable(xavier_init(self.n_input,self.n_hidden))
+        all_weights['b1']=tf.Variable(tf.zeros([self.n_hidden],dtype=tf.float32))
+        all_weights['w2']=tf.Variable(tf.zeros([self.n_hidden,self.n_input],dtype=tf.float32))
+        all_weights['b2']=tf.Varibale(tf.zeros([self.n_input],dtype=tf.float32))
+        return all_weights
 
-        # A function which calculates the cost function and executive one training step.
-        def partial_fit(self,X):
-            cost,opt=self.sess.run((self.cost,self.optimizer),feed_dict={self.x:X,self.scale:self.training_scale})
-            return cost
+    # A function which calculates the cost function and executive one training step.
+    def partial_fit(self,X):
+        cost,opt=self.sess.run((self.cost,self.optimizer),feed_dict={self.x:X,self.scale:self.training_scale})
+        return cost
 
-        # A funtion calculating cost function only.
-        def calc_total_cost(self,X):
-            return self.sess.run(self.cost,feed_dict={self.x:X,self.scale:self.training_sccale})
+    # A funtion calculating cost function only.
+    def calc_total_cost(self,X):
+        return self.sess.run(self.cost,feed_dict={self.x:X,self.scale:self.training_sccale})
 
-        # One can get the features extracted by encoder through the fuction.
-        def transform(self,X):
-            return self.sess.run(self.hidden,feed_dict={self.x:X,self.scale:self.training_scale})
+    # One can get the features extracted by encoder through the fuction.
+    def transform(self,X):
+        return self.sess.run(self.hidden,feed_dict={self.x:X,self.scale:self.training_scale})
 
-        # One can reconstruct the data from features through the function.
-        def generate(self,hidden=None):
-            if hidden is None:
-                hidden=np.random.normal(size-self.weights['b1'])
-            return self.sess.run(self.reconstruction,feed_dict={self.hidden:hidden})
+    # One can reconstruct the data from features through the function.
+    def generate(self,hidden=None):
+        if hidden is None:
+            hidden=np.random.normal(size-self.weights['b1'])
+        return self.sess.run(self.reconstruction,feed_dict={self.hidden:hidden})
 
-        
-        def reconstrunction(self,X):
-            return self.sess.run(self.reconstrunction,feed_dict={self.x:X,self.scale:self.training_scale})
 
-        def getWeights(self):
-            return self.sess.run(self.weights['w1'])
+    def reconstrunction(self,X):
+        return self.sess.run(self.reconstrunction,feed_dict={self.x:X,self.scale:self.training_scale})
 
-        def getBiases(self):
-            return self.sess.run(self.weights['b1'])
+    def getWeights(self):
+        return self.sess.run(self.weights['w1'])
+
+    def getBiases(self):
+        return self.sess.run(self.weights['b1'])
